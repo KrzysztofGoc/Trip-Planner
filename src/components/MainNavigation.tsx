00bf98667c6721b2a0f703c50d483ba0fad15712
Krzysztof <87522326+KrzysztofGoc@users.noreply.github.com>
2025-02-26 16:40:36 +0100

    feat: Add ShadCN buttons with Lucide icons to mobile navigation bar

--- src/components/MainNavigation.tsx
+++ src/components/MainNavigation.tsx
@@ -1,9 +1,32 @@
+import { PlusCircle, User, Luggage } from "lucide-react";
+import { Button } from "./ui/button";
 
 export default function MainNavigation() {
 
     return (
-        <>
-            <div>Main Navigation</div>
-        </>
+        /* Mobile Navigation */
+        <div className="fixed bottom-0 left-0 w-full bg-gray-900 text-white p-4">
+            <div className="grid grid-cols-3 gap-4">
+
+                {/* Trips Button */}
+                <Button variant="ghost"> 
+                    <Luggage /> 
+                    Trips
+                </Button>
+
+                {/* Add Trip Button */}
+                <Button variant="ghost" > 
+                    <PlusCircle /> 
+                    Add Trip
+                </Button>
+
+                {/* User Profile Button */}
+                <Button variant="ghost" > 
+                    <User /> 
+                    Profile
+                </Button>
+
+            </div>
+        </div>
     )
 }
