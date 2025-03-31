a00b618b87cddfe8beade0483e465c3e3dd303f2
Krzysztof <87522326+KrzysztofGoc@users.noreply.github.com>
2025-03-31 17:02:51 +0200

    feat: Setup react-google-maps library to provide maps for trips

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,12 +1,17 @@
-import 'leaflet/dist/leaflet.css';
 import { Place } from '@/types/place';
 import { TripEvent } from '@/types/tripEvent';
+import { Map } from '@vis.gl/react-google-maps';
 
 
 type MapProps = { mode: 'place'; place: Place } | { mode: 'route'; events: TripEvent[] };
 
 export default function MapComponent(props: MapProps) {
   return (
-    <p>Map Component</p>
+    <Map
+      mapId="" // Optional - for custom styled maps
+      style={{ width: '100%', height: '500px' }}
+      defaultCenter={{ lat: 35.6895, lng: 139.6917 }}
+      defaultZoom={10}
+    />
   );
 }
