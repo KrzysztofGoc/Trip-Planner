9a95f029622255773dd06c537019f81be4a9846f
Krzysztof <87522326+KrzysztofGoc@users.noreply.github.com>
2025-04-01 17:05:38 +0200

    feat: Implement Map component with place and route support

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,17 +1,60 @@
 import { Place } from '@/types/place';
 import { TripEvent } from '@/types/tripEvent';
-import { Map } from '@vis.gl/react-google-maps';
+import { Map, AdvancedMarker, MapControl, ControlPosition } from '@vis.gl/react-google-maps';
+import Directions from './Directions';
 
-
-type MapProps = { mode: 'place'; place: Place } | { mode: 'route'; events: TripEvent[] };
+type MapProps =
+  | { mode: 'place'; place: Place }
+  | { mode: 'route'; events: TripEvent[] };
 
 export default function MapComponent(props: MapProps) {
+  const isPlaceMode = props.mode === 'place';
+  const isRouteMode = props.mode === 'route';
+  const events = isRouteMode ? props.events : [];
+
+  let center = { lat: 0, lng: 0 };
+  let content: React.ReactNode = null;
+
+  // When map is showing one place
+  if (isPlaceMode) {
+    center = { lat: props.place.lat, lng: props.place.lng };
+    content = <AdvancedMarker position={center} />;
+  }
+
+  // When map is showing day's route
+  if (isRouteMode) {
+    // When there are no events in selected day display error message
+    if (events.length === 0) {
+      content = (
+        <MapControl position={ControlPosition.CENTER}>
+          <div className="bg-white rounded p-4 shadow">
+            <p className="font-semibold text-lg">
+              No events for this day
+            </p>
+          </div>
+        </MapControl>
+      );
+      // When there is 1 event in selected day display single marker
+    } else if (events.length === 1) {
+      center = { lat: events[0].lat, lng: events[0].lng };
+      content = <AdvancedMarker position={{ lat: events[0].lat, lng: events[0].lng }}
+      />
+      // When there are at least 2 events display route
+    } else if (events.length > 1) {
+      center = { lat: events[0].lat, lng: events[0].lng };
+      content = <Directions events={events} />;
+    }
+  }
+
   return (
-    <Map
-      mapId="" // Optional - for custom styled maps
-      style={{ width: '100%', height: '500px' }}
-      defaultCenter={{ lat: 35.6895, lng: 139.6917 }}
-      defaultZoom={10}
-    />
+    <div className="w-screen h-screen">
+      <Map
+        defaultZoom={12}
+        defaultCenter={center}
+        mapId={import.meta.env.VITE_GOOGLE_MAP_ID}
+      >
+        {content}
+      </Map>
+    </div>
   );
 }
