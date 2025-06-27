448ecb693a9d7c5192c6d85257c8e5c41deec0e2
Krzysztof <87522326+KrzysztofGoc@users.noreply.github.com>
2025-06-27 13:49:12 +0200

    feat: Add optimstic updates to trip image updating

--- src/components/Trip/TripImageChangeDialog.tsx
+++ src/components/Trip/TripImageChangeDialog.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { updateTripImage } from "@/api/trips";
 import { IMAGE_PRESETS } from "@/constants/tripImages";
 import { queryClient } from "@/api/queryClient";
+import { Trip } from "@/types/trip";
 
 interface TripImageChangeDialogProps {
   open: boolean;
@@ -17,22 +18,55 @@ export default function TripImageChangeDialog({ open, onClose, tripId }: TripIma
   const [selectedUrl, setSelectedUrl] = useState<string | null>(null);
 
   const { mutate: updateImage, isPending } = useMutation({
-    mutationFn: () => updateTripImage({ tripId, imageUrl: selectedUrl }),
-    onError: () => toast.error("Failed to update image", { id: "update-trip-image" }),
-    onSuccess: () => {
-      toast.success("Image updated", { id: "update-trip-image" });
-      queryClient.invalidateQueries({ queryKey: ["trips", { tripId: tripId }] });
-      setSelectedUrl(null);
-      onClose();
+    mutationFn: updateTripImage,
+
+    // Optimistic update
+    onMutate: async () => {
+      toast.success("Trip image updated", { id: "update-trip-image" });
+
+      // Cancel ongoing trip queries
+      await queryClient.cancelQueries({ queryKey: ["trips", { tripId }] });
+
+      // Snapshot previous data
+      const previousTripData = queryClient.getQueryData<Trip>(["trips", { tripId }]);
+      if (!previousTripData) {
+        throw new Error("Cannot optimistically update: trip data is missing from cache");
+      }
+
+      // Optimistically update image in cache
+      queryClient.setQueryData(["trips", { tripId }], {
+        ...previousTripData,
+        image: selectedUrl,
+      });
+
+      // Pass snapshot for rollback
+      return { previousTripData };
+    },
+
+    // Rollback on error
+    onError: (_err, _data, context) => {
+      toast.error("Failed to update image", { id: "update-trip-image" });
+
+      const previousTripData = context?.previousTripData;
+      if (previousTripData) {
+        queryClient.setQueryData(["trips", { tripId }], previousTripData);
+      }
+    },
+
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ["trips", { tripId }] });
     },
   });
 
-  function handleImageClick({ url }: { url: string }) {
-    setSelectedUrl(url);
+  function handleUpdateImage() {
+    updateImage({ tripId: tripId, imageUrl: selectedUrl });
+    setSelectedUrl(null)
+    onClose();
   }
 
-  function handleUpdateImage() {
-    updateImage();
+  function handleCancel() {
+    setSelectedUrl(null)
+    onClose();
   }
 
   return (
@@ -46,7 +80,7 @@ export default function TripImageChangeDialog({ open, onClose, tripId }: TripIma
           {IMAGE_PRESETS.map(({ key, label, url }) => (
             <button
               key={key}
-              onClick={() => handleImageClick({ url: url })}
+              onClick={() => setSelectedUrl(url)}
               className={`border-2  ${selectedUrl === url ? "border-red-400" : "border-transparent"}`}
             >
               <img src={url} alt={label} className="object-cover size-full" />
@@ -57,16 +91,16 @@ export default function TripImageChangeDialog({ open, onClose, tripId }: TripIma
         <DialogFooter className="flex justify-end gap-2 mt-4">
           <Button
             variant="secondary"
-            onClick={onClose}
+            onClick={handleCancel}
             disabled={isPending}
-            className="w-auto h-12 bg-gray-100 text-red-400 shadow-md rounded-lg"
+            className="h-12 bg-transparent shadow-none border-none text-black rounded-lg"
           >
             Cancel
           </Button>
           <Button
             onClick={handleUpdateImage}
             disabled={!selectedUrl || isPending}
-            className="w-auto h-12 bg-red-400 text-white shadow-md rounded-lg"
+            className="w-auto h-12 bg-red-400 text-white rounded-lg"
           >
             Confirm
           </Button>
