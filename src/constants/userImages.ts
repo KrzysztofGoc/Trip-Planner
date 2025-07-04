8cfb23aceb868be984a0c45f04903dcaebe3ae26
Krzysztof <87522326+KrzysztofGoc@users.noreply.github.com>
2025-07-04 16:18:40 +0200

    feat(profile): Major profile UX/UI improvements, editable name & avatar, enhanced state management
    - Redesign profile page for a more modern and intuitive user experience
    - Add image/avatar changing with a set of predefined images
    - Enable changing profile name inline and add improved loading states
    - Move logout logic to useMutation for better error handling and mutation feedback
    - Add working change password button to the profile
    - Show loading indicator during avatar updates for improved user feedback
    
    This update provides a smoother, more interactive, and visually appealing profile management experience.

---
+++ src/constants/userImages.ts
@@ -0,0 +1,11 @@
+export const AVATAR_PRESETS = [
+  { key: "camera", url: "/avatars/camera.png" },
+  { key: "globe", url: "/avatars/globe.png" },
+  { key: "mountain", url: "/avatars/mountain.png" },
+  { key: "paper_airplane", url: "/avatars/paper_airplane.png" },
+  { key: "suitcase", url: "/avatars/suitcase.png" },
+  { key: "baloon", url: "/avatars/baloon.png" },
+  { key: "compass", url: "/avatars/compass.png" },
+  { key: "map", url: "/avatars/map.png" },
+  { key: "beach", url: "/avatars/beach.png" },
+];
