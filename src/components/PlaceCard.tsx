6136ed88583a99185bd71fa4367d4038fa681483
Krzysztof <87522326+KrzysztofGoc@users.noreply.github.com>
2025-07-16 10:35:26 +0200

    feat: Improve PlacesListPage UI and functionality
    - Handle case when places search returns empty array and display separate message.
    - Filter places by selected category using includedPrimaryTypes in searchNearby API.
    - Implement lazy loading of PlaceCards in PlacesGrid for better performance.
    - Add max height constraint to cover images in TripImage component.
    - Fix issue with simultaneous display of "No trips found" and "Loading trips..." messages on TripsPage.
    - Move lazy loading of PlaceCards inside PlaceCard component for universal functionality across the app.
    - Add fallback default image for PlaceCard when no image is available.

--- src/components/PlaceCard.tsx
+++ src/components/PlaceCard.tsx
@@ -1,22 +1,31 @@
+import LazyLoad from 'react-lazyload';
 import { Place } from "@/types/place";
 
 interface PlaceCardProps {
-    event: Place,
+    event: Place;
     onClick?: () => void;
 }
 
 export default function PlaceCard({ event, onClick }: PlaceCardProps) {
     return (
-        <div onClick={onClick} role="button" className="border-l-4 border-red-400 bg-gray-50 flex shadow-md rounded-lg h-fit">
-            <img src="https://images.unsplash.com/photo-1667840578874-ef842d59c824" className="size-32 aspect-square object-cover rounded-l-sm" />
-            <div className="flex flex-col p-4 pr-12 justify-center grow">
-                <p className="text-md font-bold select-none">
-                    {event.name}
-                </p>
-                <p className="text-gray-500 font-light text-sm select-none">
-                    {event.category}
-                </p>
+        <LazyLoad
+            height={128}
+            offset={-200}
+            once
+            placeholder={<div className="h-32 animate-pulse bg-gray-300 rounded-lg" />}
+        >
+            <div onClick={onClick} role="button" className="border-l-4 border-red-400 bg-gray-50 flex shadow-md rounded-lg h-fit">
+                {/* Image */}
+                <img
+                    src={"/place_default_thumbnail_image.png"} // Add placeholder or fallback image
+                    className="size-32 aspect-square object-cover rounded-l-sm"
+                    alt={event.name}
+                />
+                <div className="flex flex-col p-4 pr-12 justify-center grow">
+                    <p className="text-md font-bold select-none">{event.name}</p>
+                    <p className="text-gray-500 font-light text-sm select-none">{event.category}</p>
+                </div>
             </div>
-        </div>
+        </LazyLoad>
     );
 }
