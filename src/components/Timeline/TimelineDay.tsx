083e781a086c6a600a86da55428e17ae9976491a
Krzysztof <87522326+KrzysztofGoc@users.noreply.github.com>
2025-07-17 17:56:00 +0200

    feat: Make TripTimeline days collapsible and refactor event fetching
    - Make TripTimeline days collapsible to save space; first day with events is expanded by default
    - Move trip events fetching logic to TripPage to provide a single source of truth

--- src/components/Timeline/TimelineDay.tsx
+++ src/components/Timeline/TimelineDay.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export default function TimelineDay({ children }: PropsWithChildren) {
     return (
-        <div className="size-auto flex flex-col gap-4">
+        <div className="size-auto flex flex-col gap-4 pb-2">
             {children}
         </div>
     );
